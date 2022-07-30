2c8fc9028eca34bdd412c20777d4d4c53a36f6f4
hrdtbs <harada.tsubasa.0422@gmail.com>
2022-07-30 18:45:50 +0900

    refactor: anyhow

--- src/executers/config.rs
+++ src/executers/config.rs
@@ -6,28 +6,26 @@ use std::{fs::File, io::Write};
 const CONFIG_FILE_NAME: &str = "endpoints.config.json";
 
 pub fn write_config_file(config: Config) -> Result<()> {
-    let mut file = File::create(CONFIG_FILE_NAME).expect("Config file not found");
+    let mut file = File::create(CONFIG_FILE_NAME)?;
 
-    serde_json::to_writer_pretty(&mut file, &config).expect("Failed to write config file");
+    serde_json::to_writer_pretty(&mut file, &config)?;
     Ok(())
 }
 
 pub fn create_config_file() -> Result<()> {
-    let mut output: File = File::create(CONFIG_FILE_NAME).unwrap();
+    let mut output: File = File::create(CONFIG_FILE_NAME)?;
     let config = Config::new(ConfigOption {
         ..Default::default()
     })
-    .publish()
-    .unwrap();
-    output.write_all(config.as_bytes()).unwrap();
+    .publish()?;
+    output.write_all(config.as_bytes())?;
     Ok(())
 }
 
 pub fn read_config_file() -> Result<Config> {
-    let mut file = File::open(CONFIG_FILE_NAME).expect("Config file not found");
+    let mut file = File::open(CONFIG_FILE_NAME)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Couldn't read config file");
-    let config: Config = serde_json::from_str(&contents).unwrap();
+    file.read_to_string(&mut contents)?;
+    let config: Config = serde_json::from_str(&contents)?;
     Ok(config)
 }
